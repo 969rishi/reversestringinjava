0d71953527ef5437509d7d2e2b9203897bc29615
Rishi <32278183+969rishi@users.noreply.github.com>
2020-01-10 14:10:46 +0530

    create reversestring.java
    how to reverse a string in java

---
+++ reversestring.java
@@ -0,0 +1,11 @@
+ for (int i = 0; i < wordPlay.length(); i++) {
+            wordPlay.charAt(i)
+        }
+        
+        
+        char[] holdme = wordPlay.toCharArray();
+        String playWord = "";
+        for (int i = 0; i < holdme.length; i++) {
+            playWord = holdme[i] + playWord;
+        }
+        System.Out.Println("")
